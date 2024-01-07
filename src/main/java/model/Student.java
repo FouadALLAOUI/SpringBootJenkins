e9594a21ab5b07a627ccfb4ff385c17871b61d98
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-01-07 22:19:04 +0100

    Add Class Student class

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -3,6 +3,7 @@ package model;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 public class Student {
+    //Add a comment
     private String FirstName;
     private String LastName;
     private int age;
